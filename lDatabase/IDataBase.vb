6bc8cd43c320c518abbfb4162b11621bb6907e18
lity2k <lity2k@gmail.com>
2016-06-01 17:08:40 +0800

    test

---
+++ lDatabase/IDataBase.vb
@@ -0,0 +1,32 @@
+﻿Public Interface IDataBase
+    ''' <summary>
+    ''' 获取数据库连接字串
+    ''' </summary>
+    ''' <returns></returns>
+    Property connectionString As String
+
+    ''' <summary>
+    ''' 执行sql查询语句，并返回datatable。
+    ''' </summary>
+    ''' <param name="sql"></param>
+    ''' <param name="Parameters"></param>
+    ''' <returns></returns>
+    Function ExecuteDataTable(sql As String, Optional Parameters As Dictionary(Of String, Object) = Nothing) As DataTable
+
+    ''' <summary>
+    ''' 执行sql语句，并返回受影响行数。
+    ''' </summary>
+    ''' <param name="sql"></param>
+    ''' <param name="Parameters"></param>
+    ''' <returns></returns>
+    Function ExecuteNonQuery(sql As String, Optional Parameters As Dictionary(Of String, Object) = Nothing) As Integer
+
+    ''' <summary>
+    ''' 执行sql语句，结果集中第一行的第一列。
+    ''' </summary>
+    ''' <param name="sql"></param>
+    ''' <param name="Parameters"></param>
+    ''' <returns></returns>
+    Function ExecuteScalar(sql As String, Optional Parameters As Dictionary(Of String, Object) = Nothing) As Object
+
+End Interface
